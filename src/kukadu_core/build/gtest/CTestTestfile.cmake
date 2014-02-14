4154d005fdb1e27d1915c401ad5fadc2178808c4
simon hangl <simon.hangl@uibk.ac.at>
2014-02-14 11:56:51 +0100

    init

--- src/kukadu_core/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/shangl/catkin_ws/src/kukadu/src/kukadu_core/build/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
