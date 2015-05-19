d071c7363cce3bce9f1c3ac9eaf4195d09353f14
simon hangl <simon.hangl@uibk.ac.at>
2015-05-19 14:17:51 +0200

    fixed some namespaces

--- src/robot/PlottingControlQueue.h
+++ src/robot/PlottingControlQueue.h
@@ -28,13 +28,13 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/MultiArrayDimension.h>
 #include <geometry_msgs/Pose.h>
-#include <iis_kukie/CartesianImpedance.h>
-#include <iis_kukie/FriJointCommand.h>
-#include <iis_kukie/FriJointImpedance.h>
-#include <iis_kukie/FriJointState.h>
-#include <iis_kukie/FriRobotData.h>
-#include <iis_kukie/FriRobotJntData.h>
-#include <iis_kukie/KukieError.h>
+#include <iis_robot_dep/CartesianImpedance.h>
+#include <iis_robot_dep/FriJointCommand.h>
+#include <iis_robot_dep/FriJointImpedance.h>
+#include <iis_robot_dep/FriJointState.h>
+#include <iis_robot_dep/FriRobotData.h>
+#include <iis_robot_dep/FriRobotJntData.h>
+#include <iis_robot_dep/KukieError.h>
 
 #define COMMAND_NOT_SET -100
 
