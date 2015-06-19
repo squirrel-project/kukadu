d6df88333307753adb3018a01d86afd5f0d16830
simon hangl <simon.hangl@uibk.ac.at>
2015-06-19 14:05:54 +0200

    merged senkas stuff

--- src/manipulation/pushing/DMPExecutorPush.cpp
+++ src/manipulation/pushing/DMPExecutorPush.cpp
@@ -1,8 +1,9 @@
 #include "DMPExecutorPush.h"
 
-DMPExecutorPush::DMPExecutorPush(Dmp dmp, std::shared_ptr<ControlQueue> execQueue, int doSimulation, std::shared_ptr<SimInterface> simI,string object_id): DMPExecutor(dmp, execQueue), object_id(object_id), doSimulation(doSimulation), simI(simI)
-{}
-double DMPExecutorPush::addTerm(double t, const double* currentDesiredYs, int jointNumber, std::shared_ptr<ControlQueue> queue){
+DMPExecutorPush::DMPExecutorPush(Dmp dmp, std::shared_ptr<ControlQueue> execQueue, int doSimulation, std::shared_ptr<SimInterface> simI,string object_id): DMPExecutor(dmp, execQueue), object_id(object_id), doSimulation(doSimulation), simI(simI) {
 
-        return 0;
-    }
+}
+
+double DMPExecutorPush::addTerm(double t, const double* currentDesiredYs, int jointNumber, std::shared_ptr<ControlQueue> queue) {
+    return 0;
+}
--- src/manipulation/pushing/DMPExecutorPush.h
+++ src/manipulation/pushing/DMPExecutorPush.h
@@ -18,16 +18,18 @@ using namespace arma;
 
 class DMPExecutorPush : public DMPExecutor {
 private:
+
     string object_id;
     std::shared_ptr<SimInterface>  simI;
     int doSimulation;
     mat DesPos;
 
-
-
 public:
+
     DMPExecutorPush(Dmp dmp, std::shared_ptr<ControlQueue> execQueue, int doSimulation, std::shared_ptr<SimInterface> simI,string object_id);
-    double addTerm(double t, const double* currentDesiredYs, int jointNumber, std::shared_ptr<ControlQueue> queue)override ;
+
+    double addTerm(double t, const double* currentDesiredYs, int jointNumber, std::shared_ptr<ControlQueue> queue) override;
+
 };
 
 #endif // DMPEXECUTORPUSH_H
