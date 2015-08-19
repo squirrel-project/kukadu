bc68d9d6b63c7a3a51cf0af8166d239eddca0d10
simon hangl <simon.hangl@uibk.ac.at>
2015-08-19 11:29:20 +0200

    merged senkas stuff (senka please test)

--- src/icra2016.cpp
+++ src/icra2016.cpp
@@ -11,44 +11,15 @@
 #include "learning/projective_simulation/core/projectivesimulator.h"
 #include "learning/projective_simulation/application/neverendingcolorreward.h"
 
-/*
-#include <pcl/common/common.h>
-#include <pcl/common/transforms.h>
-#include <pcl/common/pca.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-*/
-
 namespace po = boost::program_options;
 namespace pf = boost::filesystem;
 
 using namespace std;
 
-//using namespace pcl;
-
-/*
-struct FitCube {
-    Eigen::Vector3f translation;
-    Eigen::Quaternionf rotation;
-    double width, height, depth;
-};
-*/
-
-/*
-PointCloud<PointXYZ>::Ptr segmentPlanar(PointCloud<PointXYZ>::Ptr cloud, bool negative);
-FitCube fitBox(PointCloud<PointXYZ>::Ptr cloud);
-*/
-
 string environment = "simulation";
 
 string tmpDataFolder = resolvePath("$HOME/tmp/");
-string pushDataFolder = resolvePath("$KUKADU_HOME/movements/iros2015/");
+string pushDataFolder = resolvePath("$KUKADU_HOME/movements/icra2016/");
 
 int doHapticTest(std::shared_ptr<ControlQueue> leftQueue, shared_ptr<RosSchunk> leftHand);
 
@@ -218,32 +189,34 @@ int main(int argc, char** args) {
     cout << "all properties loaded" << endl;
     int kukaStepWaitTime = dmpStepSize * 1e6;
 
+    cout << "1" << endl;
+
     ros::init(argc, args, "kukadu"); ros::NodeHandle* node = new ros::NodeHandle(); usleep(1e6);
     shared_ptr<ControlQueue> leftQueue = shared_ptr<ControlQueue>(new KukieControlQueue(kukaStepWaitTime, environment, "left_arm", *node));
     shared_ptr<ControlQueue> rightQueue = shared_ptr<ControlQueue>(new KukieControlQueue(kukaStepWaitTime, environment, "right_arm", *node));
-
+cout << "2" << endl;
     shared_ptr<RosSchunk> leftHand = shared_ptr<RosSchunk>(new RosSchunk(*node, environment, "left"));
     shared_ptr<RosSchunk> rightHand = shared_ptr<RosSchunk>(new RosSchunk(*node, environment, "right"));
-
+cout << "3" << endl;
     vector<shared_ptr<ControlQueue>> queueVectors;
     queueVectors.push_back(leftQueue);
-
+cout << "4" << endl;
     learnFinalPush(leftQueue);
     learnRotHor(leftQueue);
     learnRotVert(leftQueue);
     learnTransBody(leftQueue);
     learnTransRight(leftQueue);
     learnTransLeft(leftQueue);
-
+cout << "5" << endl;
     leftQueue->stopCurrentMode();
     leftQueue->switchMode(KukieControlQueue::KUKA_JNT_IMP_MODE);
-
+cout << "6" << endl;
     rightQueue->stopCurrentMode();
     rightQueue->switchMode(KukieControlQueue::KUKA_JNT_IMP_MODE);
-
+cout << "7" << endl;
     shared_ptr<thread> lqThread = leftQueue->startQueueThread();
     shared_ptr<thread> rqThread = rightQueue->startQueueThread();
-
+cout << "8" << endl;
     int hapticCat = -1;
 
     while(hapticCat != 2) {
@@ -452,11 +425,15 @@ void goToStartPos(std::shared_ptr<ControlQueue> leftQueue) {
 }
 
 void learnFinalPush(std::shared_ptr<ControlQueue> leftQueue) {
-
+    cout << "a" << endl;
     dataFinalPush = SensorStorage::readStorage(leftQueue, pushDataFolder + "finalPush/kuka_lwr_real_left_arm_0");
+    cout << "b" << endl;
     timesFinalPush = dataFinalPush->getTimes();
+    cout << "c" << endl;
     learnerFinalPush = shared_ptr<JointDMPLearner>(new JointDMPLearner(az, bz, join_rows(timesFinalPush, dataFinalPush->getJointPos())));
+    cout << "d" << endl;
     dmpFinalPush = learnerFinalPush->fitTrajectories();
+    cout << "e" << endl;
 
 }
 
@@ -580,96 +557,3 @@ void executeTransBody(std::shared_ptr<ControlQueue> leftQueue, double execTime) 
     execTransBody.executeTrajectory(ac, 0, execTime, dmpStepSize, tolAbsErr, tolRelErr);
 
 }
-
-
-/*
-// according to http://www.pcl-users.org/Finding-oriented-bounding-box-of-a-cloud-td4024616.html
-FitCube fitBox(PointCloud<PointXYZ>::Ptr cloud) {
-
-    FitCube retCube;
-    PCA<PointXYZ> pca;
-    PointCloud<PointXYZ> proj;
-
-    pca.setInputCloud(cloud);
-    pca.project(*cloud, proj);
-
-    PointXYZ proj_min;
-    PointXYZ proj_max;
-    getMinMax3D(proj, proj_min, proj_max);
-
-    PointXYZ min;
-    PointXYZ max;
-    pca.reconstruct(proj_min, min);
-    pca.reconstruct(proj_max, max);
-    std::cout << " min.x= " << min.x << " max.x= " << max.x << " min.y= " << min.y << " max.y= " << max.y << " min.z= " << min.z << " max.z= " << max.z << std::endl;
-
-    //Rotation of PCA
-    Eigen::Matrix3f rot_mat = pca.getEigenVectors();
-
-    //translation of PCA
-    Eigen::Vector3f cl_translation = pca.getMean().head(3);
-
-    Eigen::Matrix3f affine_trans;
-    std::cout << rot_mat << std::endl;
-    //Reordering of principal components
-    affine_trans.col(2) << (rot_mat.col(0).cross(rot_mat.col(1))).normalized();
-    affine_trans.col(0) << rot_mat.col(0);
-    affine_trans.col(1) << rot_mat.col(1);
-    //affine_trans.col(3) << cl_translation,1;
-
-    std::cout << affine_trans << std::endl;
-
-    retCube.rotation = Eigen::Quaternionf(affine_trans);
-    Eigen::Vector4f t = pca.getMean();
-
-    retCube.translation = Eigen::Vector3f(t.x(), t.y(), t.z());
-
-    retCube.width = fabs(proj_max.x-proj_min.x);
-    retCube.height = fabs(proj_max.y-proj_min.y);
-    retCube.depth = fabs(proj_max.z-proj_min.z);
-
-    return retCube;
-
-}
-
-PointCloud<PointXYZ>::Ptr segmentPlanar(PointCloud<PointXYZ>::Ptr cloud, bool negative) {
-
-    // Create the segmentation object for the planar model and set all the parameters
-    PointCloud<PointXYZ>::Ptr cloud_f(new PointCloud<PointXYZ>);
-    SACSegmentation<PointXYZ> seg;
-    PointIndices::Ptr inliers(new PointIndices);
-    ModelCoefficients::Ptr coefficients(new ModelCoefficients);
-    PointCloud<PointXYZ>::Ptr cloud_plane(new PointCloud<PointXYZ> ());
-
-    seg.setOptimizeCoefficients(true);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(100);
-    seg.setDistanceThreshold(0.02);
-
-    // Segment the largest planar component from the remaining cloud
-    seg.setInputCloud(cloud);
-    seg.segment(*inliers, *coefficients);
-    if (inliers->indices.size () == 0) {
-        cerr << "Could not estimate a planar model for the given dataset." << endl;
-        return cloud_f;
-    }
-
-    // Extract the planar inliers from the input cloud
-    ExtractIndices<PointXYZ> extract;
-    extract.setInputCloud(cloud);
-    extract.setIndices(inliers);
-    extract.setNegative(false);
-
-    // Get the points associated with the planar surface
-    extract.filter(*cloud_plane);
-    cout << "PointCloud representing the planar component: " << cloud_plane->points.size() << " data points." << endl;
-
-    // Remove the planar inliers, extract the rest
-    extract.setNegative(negative);
-    extract.filter(*cloud_f);
-
-    return cloud_f;
-
-}
-*/
